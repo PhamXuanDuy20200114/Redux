d1cf277b1ca3ff2f99e543dfc8d2d99a77109cd1
= <=>
2024-03-12 07:37:42 +0700

    Redux with class cpn vs hook

--- src/App.js
+++ src/App.js
@@ -2,35 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './action/actions';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App(props) {
+  const dispatch = useDispatch();
+
+  const newCount = useSelector(
+    (state) => { return state.counter.count }
+  )
+
+  const handleIncrease = () => {
+    //props.increaseCounter(); // using mapDispatchToProps Class Component
+    dispatch(increaseCounter()); // using useDispatch Hook
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <div>Count: {props.count}</div>
+        <div>Count: {newCount}</div>
 
-        <button onClick={() => props.increaseCounter()}>Increase Count</button>
+        <button onClick={() => handleIncrease()}>Increase Count</button>
 
-        <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
       </header>
     </div>
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    count: state.counter.count,
-  }
-}
+// const mapStateToProps = state => {
+//   return {
+//     count: state.counter.count,
+//   }
+// }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  }
-}
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+export default App;
